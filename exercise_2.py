399cb3ca7b5b90da54d40dc77784998f1c766e40
Jongmin Lee <starjongmin@gmail.com>
2019-07-07 21:44:59 +0900

    rename exercise

--- exercise_2.py
+++ exercise_2.py
@@ -3,13 +3,12 @@ import time
 import gym
 import envs
 import numpy as np
-import matplotlib.pyplot as plt
 
 np.set_printoptions(precision=3, suppress=True, threshold=10000, linewidth=250)
 
 """ Load environment """
-env_name = 'MazeSample3x3-v0'
-# env_name = 'MazeSample5x5-v0'
+env_name = 'MazeSample5x5-v0'
+# env_name = 'MazeSample10x10-v0'
 # env_name = 'MazeRandom10x10-v0'
 # env_name = 'MazeRandom10x10-plus-v0'
 # env_name = 'MazeRandom20x20-v0'
@@ -18,7 +17,6 @@ env_name = 'MazeSample3x3-v0'
 # env_name = 'MyCartPole-v0'
 env = gym.make(env_name)
 env.S, env.A, env.T, env.R, env.gamma = env.env.S, env.env.A, env.env.T, env.env.R, env.env.gamma
-env.draw_policy_evaluation = env.env.draw_policy_evaluation
 
 """
 env.S: the number of states (integer)
@@ -45,28 +43,42 @@ def policy_evaluation(env, pi):
 
     return V, Q
 
-pi = np.ones((env.S, env.A)) / env.A
-# pi = np.random.dirichlet(np.ones(env.A), env.S)
-pi = np.zeros((env.S, env.A))
-pi[:, 0] = 1
-# pi = np.array([[0, 1, 0, 0],
-#                [0, 0, 0, 1],
-#                [0, 0, 0, 1],
-#                [0, 1, 0, 0],
-#                [0, 0, 1, 0],
-#                [1, 0, 0, 0],
-#                [0, 0, 1, 0],
-#                [0, 0, 1, 0],
-#                [1, 0, 0, 0],
-#                [0.25, 0.25, 0.25, 0.25]])
 
-V, Q = policy_evaluation(env, pi)
+def policy_improvement(env, Q):
+    pi = np.zeros((env.S, env.A))
+    pi[np.arange(env.S), np.argmax(Q, axis=1)] = 1.
+    return pi
+
+
+pi = np.ones((env.S, env.A)) / env.A
+for i in range(1000):
+    V, Q = policy_evaluation(env, pi)
+    new_pi = policy_improvement(env, Q)
+    if np.all(pi == new_pi):
+        break
+    pi = new_pi
+    print(i, pi)
 print(Q)
 # print(pi)
 
-# env.reset()
-# print(env.step(2))
-env.draw_policy_evaluation(pi, Q)
-for i in range(600):
+for episode in range(10):
+    state = env.reset()
     env.render()
-    time.sleep(0.1)
+
+    episode_reward = 0.
+    for t in range(1000):
+        action = int(np.random.choice(np.arange(env.A), p=pi[state, :]))
+        state1, reward, done, info = env.step(action)
+        episode_reward += reward
+        print("[%4d] state=%4s / action=%d / reward=%7.4f / state1=%4s / info=%s" % (t, state, action, reward, state1, info))
+
+        env.render()
+        time.sleep(0.3 if 'Maze' in env_name else 0.03)
+
+        if done:
+            break
+        state = state1
+    print('Episode reward: %.4f' % episode_reward)
+
+    time.sleep(1)
+time.sleep(10)
