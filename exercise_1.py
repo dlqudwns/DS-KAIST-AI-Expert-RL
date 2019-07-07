d3828d2b0961c7f33e46af07994bd1ee695fd021
Jongmin Lee <starjongmin@gmail.com>
2019-07-07 17:13:12 +0900

    minor printoptoins

--- exercise_1.py
+++ exercise_1.py
@@ -4,17 +4,17 @@ import gym
 import envs
 import numpy as np
 
-np.set_printoptions(precision=3, suppress=True, threshold=10000)
+np.set_printoptions(precision=3, suppress=True, threshold=10000, linewidth=250)
 
 """ Load environment """
-# env_name = 'MazeSample5x5-v0'
+env_name = 'MazeSample5x5-v0'
 # env_name = 'MazeSample10x10-v0'
 # env_name = 'MazeRandom10x10-v0'
 # env_name = 'MazeRandom10x10-plus-v0'
 # env_name = 'MazeRandom20x20-v0'
 # env_name = 'MazeRandom20x20-plus-v0'
 # env_name = 'MyMountainCar-v0'
-env_name = 'MyCartPole-v0'
+# env_name = 'MyCartPole-v0'
 env = gym.make(env_name)
 env.S, env.A, env.T, env.R, env.gamma = env.env.S, env.env.A, env.env.T, env.env.R, env.env.gamma
 
@@ -70,7 +70,7 @@ for episode in range(10):
         action = int(np.random.choice(np.arange(env.A), p=pi[state, :]))
         state1, reward, done, info = env.step(action)
         episode_reward += reward
-        print("[%4d] state=%4s / action=%d / reward=%.4f / state1=%4s / info=%s" % (t, state, action, reward, state1, info))
+        print("[%4d] state=%4s / action=%d / reward=%7.4f / state1=%4s / info=%s" % (t, state, action, reward, state1, info))
 
         env.render()
         time.sleep(0.3 if 'Maze' in env_name else 0.03)
